6c115f8144ae3572eb5bd061aa1103cfd3ce353d
choo121600 <choo121600@gmail.com>
2022-06-09 16:57:15 +0900

    Feat: train & predict baseline

---
+++ models/convnxt.py
@@ -0,0 +1,20 @@
+from torch import nn
+import timm
+
+
+class ConvNxt(nn.Module):
+    def __init__(self, n_outputs:int, **kwargs):
+        super(ConvNxt, self).__init__()
+        self.model = timm.create_model('convnext_large', pretrained=True)
+        self.model.classifier = nn.Sequential(
+            nn.Linear(in_features = 1792, out_features=625),
+            nn.ReLU(),
+            nn.Dropout(p=0.3),
+            nn.Linear(in_features=625, out_features=256),
+            nn.ReLU(),
+            nn.Linear(in_features=256, out_features=n_outputs)
+        )
+        
+    def forward(self, x):
+        output = self.model(x)
+        return output
